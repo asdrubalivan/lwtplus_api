1d4249746f4cc25c2c2908472f658b6b8f2d8d03
Asdrúbal Suárez <asdrubal.ivan.suarez.rivera@gmail.com>
2016-08-20 15:08:08 -0430

    Adding texts route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var routes = {};
-['languages','words'].forEach((val) => {
+['languages','words', 'texts'].forEach((val) => {
   routes[val] = require(`./routes/${val}`);
 });
 var app = express();
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/languages',routes.languages);
 app.use('/words',routes.words);
+app.use('/texts',routes.texts);
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
---
+++ routes/texts.js
@@ -0,0 +1,92 @@
+var models = require('../models');
+
+var express = require('express');
+var _ = require('lodash');
+var router  = express.Router();
+
+function getTextParams(params) {
+  return {
+    title: params.title,
+    content: params.content,
+    media_file: params.media_file,
+    source_url: params.source_url,
+    id_language: params.id_language
+  };
+}
+
+router.get('/', (req, res) => {
+  models.Text.findAll().then((texts)=>{
+    res.send(texts);
+  },() => {
+    res.sendStatus(500);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  models.Text.find({
+    where: {
+      id: req.params.id
+    }
+  }).then((text) => {
+    if (!_.isNull(text)) {
+      res.send(text);
+    } else {
+      res.sendStatus(404);
+    }
+  }, (err) => {
+    res.status(500).send(err);
+  });
+});
+
+router.post('/', (req, res) => {
+  let textParams = getTextParams(req.body);
+  models.Text.create(textParams)
+  .then((text) => {
+    res.send(text);
+  }, (err) => {
+    res.status(500).send({
+      error: err
+    });
+  });
+});
+
+router.put('/:id', (req, res) => {
+  /*
+    TODO Change
+    express deprecated res.send(status, body): Use res.status(status).send(body) instead routes/Texts.js:50:9
+
+  */
+  let textParams = getTextParams(req.body);
+  models.Text.update(textParams,{
+    where: {
+      id: req.params.id
+    }
+  }).then((text) => {
+    res.send({
+      status: 'success',
+      Text: text,
+    }, (err) => {
+      res.sendStatus(404);
+    });
+  });
+});
+
+router.delete('/:id',(req, res) => {
+  let condition = {
+    where: {
+      id: req.params.id
+    }
+  };
+  models.Text.destroy(condition)
+  .then((destroyed) => {
+    res.send({
+      rowsDestroyed: destroyed
+    });
+  }, (err) => {
+    res.status(500).send({
+      error: err
+    });
+  });
+});
+
+module.exports = router;
