d96ca15f6a87819619b8c316be5ad8a2838bde4e
Asdrúbal Suárez <asdrubal.ivan.suarez.rivera@gmail.com>
2016-09-26 16:58:18 -0400

    Generating jwt tokens done, waiting for refactoring

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('./utils/passportutils');
+var jwt = require("jwt-simple");
+const _ = require('lodash');
 var routes = {};
 ['languages','words', 'texts'].forEach((val) => {
   routes[val] = require(`./routes/${val}`);
 });
-var app = express();
+const app = express();
+app.use(passport.initialize());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/languages',routes.languages);
 app.use('/words',routes.words);
 app.use('/texts',routes.texts);
-var port = process.env.PORT || 3000;
+app.post('/token', function (req, res) {
+    var post = _.pick(req.body, ['id', 'pass']);
+    console.log('POST', post);
+    if (post.id === 'test' && post.pass === "test") {
+        let token = jwt.encode(post, "mysecret"); //TODO Change
+        res.json({token: token});
+    } else {
+        res.sendStatus(401);
+    }
+})
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
--- utils/passportutils.js
+++ utils/passportutils.js
@@ -1,25 +1,36 @@
 'use strict';
 
-const passport = require('passport');  
-const Strategy = require('passport-local');
-
-passport.use(new Strategy(  
-  function(username, password, done) {
-    // database dummy - find user and verify password
-    // TODO Doing a auth based on database
-    if(username === 'test' && password === 'test'){
-      done(null, {
-        id: 1,
-        firstname: 'test',
-        lastname: 'test',
-        email: 'test',
-        verified: true
-      });
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require('passport');
+const _ = require('lodash');
+var opts = {}
+opts.secretOrKey = 'my-secret';
+opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    // User.findOne({id: jwt_payload.sub}, function(err, user) {
+    //     if (err) {
+    //         return done(err, false);
+    //     }
+    //     if (user) {
+    //         done(null, user);
+    //     } else {
+    //         done(null, false);
+    //         // or you could create a new account
+    //     }
+    // });
+    if (jwt_payload.id === 'test' && jwt_payload.pass === 'test') {
+        return done(null, _.pick(jwt_payload,['id', 'pass']));
+    } else {
+        return done(null, false);
     }
-    else {
-      done(null, false);
-    }
-  }
-));
+}));
 
-module.exports = passport; 
+module.exports = {
+    initialize: () => {
+        return passport.initialize();
+    },
+    authenticate: () => {
+        return passport.authenticate("jwt", {session: false});
+    }
+}
